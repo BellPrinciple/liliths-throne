58d3d2a3caf5004536ebed842cefb48d34e622b5
BellPrinciple <bell.principle@yahoo.com>
2022-03-28 09:42:34 +0200

    EncounterType is not an enum

--- src/com/lilithsthrone/game/dialogue/encounters/EncounterType.java
+++ src/com/lilithsthrone/game/dialogue/encounters/EncounterType.java
@@ -5,72 +5,71 @@ package com.lilithsthrone.game.dialogue.encounters;
  * @version 0.3.21
  * @author Innoxia, DSG
  */
-public enum EncounterType {
+public class EncounterType {
 
-	SPECIAL_DOMINION_CULTIST,
-	
-	SLAVE_USES_YOU(true),
-	SLAVE_USING_OTHER_SLAVE(false),
+	public static final EncounterType SPECIAL_DOMINION_CULTIST = new EncounterType();
+
+	public static final EncounterType SLAVE_USES_YOU = new EncounterType(true);
+	public static final EncounterType SLAVE_USING_OTHER_SLAVE = new EncounterType(false);
 	
 	// Dominion:
-	
-	DOMINION_STREET_FIND_HAPPINESS, // Kinariu
-	DOMINION_STREET_RENTAL_MOMMY,
-	DOMINION_STREET_PILL_HANDOUT,
-	
-	DOMINION_FIND_ITEM,
-	DOMINION_FIND_CLOTHING,
-	DOMINION_FIND_WEAPON,
 
-	DOMINION_ALLEY_ENFORCERS,
-	DOMINION_ALLEY_ATTACK(true),
-	DOMINION_STORM_ATTACK(true),
-	
-	DOMINION_EXPRESS_CENTAUR,
-	
-	WES_QUEST_START,
+	public static final EncounterType DOMINION_STREET_FIND_HAPPINESS = new EncounterType(); // Kinariu
+	public static final EncounterType DOMINION_STREET_RENTAL_MOMMY = new EncounterType();
+	public static final EncounterType DOMINION_STREET_PILL_HANDOUT = new EncounterType();
+
+	public static final EncounterType DOMINION_FIND_ITEM = new EncounterType();
+	public static final EncounterType DOMINION_FIND_CLOTHING = new EncounterType();
+	public static final EncounterType DOMINION_FIND_WEAPON = new EncounterType();
+
+	public static final EncounterType DOMINION_ALLEY_ENFORCERS = new EncounterType();
+	public static final EncounterType DOMINION_ALLEY_ATTACK = new EncounterType(true);
+	public static final EncounterType DOMINION_STORM_ATTACK = new EncounterType(true);
+
+	public static final EncounterType DOMINION_EXPRESS_CENTAUR = new EncounterType();
+
+	public static final EncounterType WES_QUEST_START = new EncounterType();
 	
 	
 	// Harpy nests:
-	
-	HARPY_NEST_ATTACK(true),
-//	HARPY_NEST_ATTACK_STORM(true),
-	HARPY_NEST_FIND_ITEM,
+
+	public static final EncounterType HARPY_NEST_ATTACK = new EncounterType(true);
+//	HARPY_NEST_ATTACK_STORM(true);
+public static final EncounterType HARPY_NEST_FIND_ITEM = new EncounterType();
 	
 	
 	// Submission:
-	
-	SUBMISSION_TUNNEL_ATTACK(true),
-	SUBMISSION_FIND_ITEM,
 
-	BAT_CAVERN_LURKER_ATTACK(true),
-	BAT_CAVERN_SLIME_ATTACK(true),
-	BAT_CAVERN_FIND_ITEM,
-	BAT_CAVERN_REBEL_BASE_DISCOVERED,
-	BAT_CAVERN_REBEL_PASSWORD_ONE,
-	BAT_CAVERN_REBEL_PASSWORD_TWO,
-	
-	REBEL_BASE_INSANE_SURVIVOR_ATTACK,
-	
-	VENGAR_CAPTIVE_SERVE,
-	VENGAR_CAPTIVE_GROPED,
-	VENGAR_CAPTIVE_VENGAR_FUCK,
-	VENGAR_CAPTIVE_RAT_FUCK,
-	VENGAR_CAPTIVE_ORAL_UNDER_TABLE,
-	VENGAR_CAPTIVE_GROUP_SEX,
-	
-	VENGAR_CAPTIVE_CLEAN_ROOM,
-	VENGAR_CAPTIVE_SHADOW_SILENCE_DOMINATE,
-	VENGAR_CAPTIVE_ROOM_BARRED
-	;
+	public static final EncounterType SUBMISSION_TUNNEL_ATTACK = new EncounterType(true);
+	public static final EncounterType SUBMISSION_FIND_ITEM = new EncounterType();
 
-	private boolean opportunistic;
+	public static final EncounterType BAT_CAVERN_LURKER_ATTACK = new EncounterType(true);
+	public static final EncounterType BAT_CAVERN_SLIME_ATTACK= new EncounterType(true);
+	public static final EncounterType BAT_CAVERN_FIND_ITEM = new EncounterType();
+	public static final EncounterType BAT_CAVERN_REBEL_BASE_DISCOVERED = new EncounterType();
+	public static final EncounterType BAT_CAVERN_REBEL_PASSWORD_ONE = new EncounterType();
+	public static final EncounterType BAT_CAVERN_REBEL_PASSWORD_TWO = new EncounterType();
+
+	public static final EncounterType REBEL_BASE_INSANE_SURVIVOR_ATTACK = new EncounterType();
+
+	public static final EncounterType VENGAR_CAPTIVE_SERVE = new EncounterType();
+	public static final EncounterType VENGAR_CAPTIVE_GROPED = new EncounterType();
+	public static final EncounterType VENGAR_CAPTIVE_VENGAR_FUCK = new EncounterType();
+	public static final EncounterType VENGAR_CAPTIVE_RAT_FUCK = new EncounterType();
+	public static final EncounterType VENGAR_CAPTIVE_ORAL_UNDER_TABLE = new EncounterType();
+	public static final EncounterType VENGAR_CAPTIVE_GROUP_SEX = new EncounterType();
+
+	public static final EncounterType VENGAR_CAPTIVE_CLEAN_ROOM = new EncounterType();
+	public static final EncounterType VENGAR_CAPTIVE_SHADOW_SILENCE_DOMINATE = new EncounterType();
+	public static final EncounterType VENGAR_CAPTIVE_ROOM_BARRED = new EncounterType();
+
+	private final boolean opportunistic;
 	
-	EncounterType() {
+	public EncounterType() {
 		opportunistic = false;
 	}
 	
-	EncounterType(boolean opportunistic) {
+	public EncounterType(boolean opportunistic) {
 		this.opportunistic = opportunistic;
 	}
 
