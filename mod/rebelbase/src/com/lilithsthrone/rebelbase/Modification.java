da49ebff5ba29b1c45341b5a1e5dcfab86e4227d
BellPrinciple <bell.principle@yahoo.com>
2022-03-31 08:58:47 +0200

    module com.lilithsthone.rebelbase

---
+++ mod/rebelbase/src/com/lilithsthrone/rebelbase/Modification.java
@@ -0,0 +1,17 @@
+package com.lilithsthrone.rebelbase;
+
+/**
+ * Somewhere in the bat caverns beneath Dominion you may find a hidden vault,
+ * a remainder of the world that once was.
+ * @version 0.1
+ */
+public class Modification extends com.lilithsthrone.game.Modification {
+
+	@Override
+	public void install() {
+	}
+
+	@Override
+	public void uninstall() {
+	}
+}
---
+++ mod/rebelbase/src/module-info.java
@@ -0,0 +1,4 @@
+module com.lilithsthrone.rebelbase {
+	requires com.lilithsthrone;
+	provides com.lilithsthrone.game.Modification with com.lilithsthrone.rebelbase.Modification;
+}
