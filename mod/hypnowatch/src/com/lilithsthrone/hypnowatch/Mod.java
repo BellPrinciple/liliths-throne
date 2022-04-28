5818360cdba4d4764191ec8cd6b07f9734ab5769
BellPrinciple <bell.principle@yahoo.com>
2022-04-28 19:36:54 +0200

    module hypnowatch

---
+++ mod/hypnowatch/src/com/lilithsthrone/hypnowatch/Mod.java
@@ -0,0 +1,16 @@
+package com.lilithsthrone.hypnowatch;
+
+import com.lilithsthrone.game.Modification;
+
+public class Mod extends Modification {
+
+	@Override
+	protected void install() {
+
+	}
+
+	@Override
+	protected void uninstall() {
+
+	}
+}
---
+++ mod/hypnowatch/src/module-info.java
@@ -0,0 +1,5 @@
+module com.lilithsthrone.hypnowatch {
+	requires com.lilithsthrone;
+	requires java.xml;
+	provides com.lilithsthrone.game.Modification with com.lilithsthrone.hypnowatch.Mod;
+}
